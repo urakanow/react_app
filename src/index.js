334732dd6d20a26a8293708eb6b84109a646d8e3
Юрій Коноваленко <urakanow@gmail.com>
2025-04-18 14:15:08 +0200

    added basename

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import reportWebVitals from './reportWebVitals';
 import PersonalPage from './components/PersonalPage';
 import Layout from './components/Layout';
 
+const basename = process.env.NODE_ENV === 'production' ? '/exam_postly' : '/';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Layout>
         <Routes>
           <Route path="/" element={<App /> } />
